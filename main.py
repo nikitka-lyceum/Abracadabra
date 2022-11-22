b247528473018040f9791f3f0ae959682e8f3427
output <Nh-Cinrou@yandex.ru>
2022-11-22 16:53:11 +0700

    Add File

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import random
 import sys
+
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 
@@ -7,6 +10,32 @@ class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UI.ui", self)
+
+        self.is_paint = False
+
+        self.button.clicked.connect(self.paint)
+
+    def paint(self):
+        self.is_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.is_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        for _ in range(10):
+            # Задаем кисть
+            qp.setBrush(QColor(255, 255, 0))
+            r = random.randint(5, 150)
+            x_win, y_win = self.geometry().x(), self.geometry().y()
+            x, y = random.randint(20, x_win - 20 - r - r), random.randint(20, y_win - 20 - r - r)
+            qp.drawEllipse(x, y, r, r)
+
+        self.is_paint = False
 
 
 if __name__ == '__main__':
